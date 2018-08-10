781d6749516b7182b2cf750653912550c5d4443d
Ravindra Kumar <ravidsrk@gmail.com>
2018-08-10 14:06:01 +0530

    WIP

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,24 @@
 $(document).ready(() => {
-	if (typeof web3 != "undefined") {
+	let currentContract
+	let explorerApplication
+
+	if (typeof web3 !== 'undefined') {
 		web3 = new Web3(web3.currentProvider);
 	} else {
 		web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/"));
 	}
 
-	let currentContract = new web3.eth.Contract(contractAbi, contractAddress);
-	abiDecoder.addABI(contractAbi);
-
-	const explorerApplication = new Web3Resolver(currentContract, [
-		{
-			name: "data",
-			method: "encodeABI",
-			options: {}
-		}
-	]);
+	if (typeof contractAbi !== "undefined") {
+		currentContract = new web3.eth.Contract(contractAbi, contractAddress);
+		abiDecoder.addABI(contractAbi);
+		explorerApplication = new Web3Resolver(currentContract, [
+			{
+				name: "data",
+				method: "encodeABI",
+				options: {}
+			}
+		]);
+	}
 
 	$(document).on("click", ".send-transaction", function (e) {
 		e.preventDefault();
@@ -38,18 +42,7 @@ $(document).ready(() => {
 							$("#transaction-hash").html(transactionHashTemplate({ hash: hash, }));
 						})
 						.once("receipt", function (receipt) {
-							let transactionTemplate = _.template($("#transaction-details-template").html());
-							let eventsTemplate = _.template($("#event-list-template").html());
-							const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
-
-							$("#transaction-details").html(transactionTemplate({
-								receipt: receipt,
-								inputData: data["data"]
-							}));
-
-							$("#event-details").html(eventsTemplate({
-								events: _.compact(decodedLogs)
-							}))
+							displayTransactionDetails(receipt)
 						})
 						.on("error", function (error) {
 							// TODO: Handle error by showing Dialog
@@ -60,34 +53,31 @@ $(document).ready(() => {
 	});
 
 	$(".sc-action").on("click", function (e) {
-		e.preventDefault();
-
-		$("#input-form, #transaction-hash, #transaction-details, #output, #event-details").empty();
+		e.preventDefault()
+		clearContainers()
 
 		var actionName = $(this).text();
-		var actionAbi = _.find(contractAbi, function (abi) {
-			return abi.name === actionName;
-		});
-		console.log(actionAbi);
-
-		let input = _.template($("#inputlist").html());
-
-		$("#input-form").html(input({
-			inputs: actionAbi.inputs,
-			payable: actionAbi.payable,
-			action: actionAbi.name
-		}));
+		createInputListForAction(actionName)
 	});
 
 	$(".sc-variable").on("click", function (e) {
 		e.preventDefault();
-		$("#input-form, #transaction-hash, #transaction-details, #output, #event-details").empty();
-		var actionName = $(this).text();
-		var actionAbi = _.find(contractAbi, function (abi) {
-			return abi.name === actionName;
-		});
+		clearContainers()
 
-		explorerApplication.getTransactionConfig(actionName).then(data => {
+		var variableName = $(this).text();
+		invokeEthCallRequestForMethod(variableName)
+	});
+
+	$(".sc-event").on("click", function (e) {
+		e.preventDefault();
+		clearContainers()
+
+		let eventName = $(this).text();
+		displayPastEvents(eventName);
+	})
+
+	function invokeEthCallRequestForMethod(variableName) {
+		explorerApplication.getTransactionConfig(variableName).then(data => {
 			web3.eth
 				.call({
 					to: data["address"],
@@ -100,26 +90,60 @@ $(document).ready(() => {
 					}));
 				});
 		});
-	});
+	}
 
-	$(".sc-event").on("click", function(e) {
-		$("#input-form, #transaction-hash, #transaction-details, #output, #event-details").empty();
-		let eventName = $(this).text();
-		let eventsTemplate = _.template($("#past-events-template").html());
+	function createInputListForAction(actionName) {
+		var actionAbi = _.find(contractAbi, function (abi) {
+			return abi.name === actionName;
+		});
 
-		currentContract.getPastEvents(eventName, {
-			fromBlock: 0,
-			toBlock: 'latest'
-		}, function (error, events) {
-			$("#event-details").html(eventsTemplate({
-				events: events,
-				eventName: eventName
-			}))
-		})
-	})
+		let input = _.template($("#inputlist").html());
+
+		$("#input-form").html(input({
+			inputs: actionAbi.inputs,
+			payable: actionAbi.payable,
+			action: actionAbi.name
+		}));
+	}
 
 	function removePaddedZeros(address) {
 		let numberString = web3.utils.hexToNumberString(address)
 		return web3.utils.toHex(numberString)
 	}
+
+	function displayTransactionDetails(receipt) {
+		let transactionTemplate = _.template($("#transaction-details-template").html());
+		let eventsTemplate = _.template($("#event-list-template").html());
+		const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
+
+		$("#transaction-details").html(transactionTemplate({
+			receipt: receipt,
+			inputData: data["data"]
+		}));
+
+		$("#event-details").html(eventsTemplate({
+			events: _.compact(decodedLogs)
+		}))
+	}
+
+	function displayPastEvents(eventName) {
+		currentContract.getPastEvents(eventName, {
+			fromBlock: 0,
+			toBlock: 'latest'
+		}, function (error, events) {
+			displayEvent(events, eventName)
+		})
+	}
+
+	function displayEvent(events, eventName) {
+		let eventsTemplate = _.template($("#past-events-template").html());
+		$("#event-details").html(eventsTemplate({
+			events: events,
+			eventName: eventName
+		}))
+	}
 });
+
+function clearContainers() {
+	$("#input-form, #transaction-hash, #transaction-details, #output, #event-details").empty();
+}
